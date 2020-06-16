c86849a0e6bbdd36479ca41b27dec910cac231cd
saisudhakar <muvvasaisudhakar@gmail.com>
2020-06-17 01:20:15 +0530

    single story page added
    single story added

---
+++ source/singlestory.php
@@ -0,0 +1,52 @@
+<body>
+<?php include 'header.php' ?>
+
+<div  class="container ">
+    <div  class="bg-white">
+        <div  class="d-flex flex-column col-lg-12">
+            <div  class="share_story"><b  class="story_feed"><a  href="javascript:void(0)"><i  class="fas fa-arrow-left"></i></a>&nbsp;Sweet grapes</b></div>
+            <div  class="w-100">
+                <p>A fox passed by a grapevine winding around the branches of a tree. He saw a bunch of grapes hanging above. He jumped to snatch some grapes. But the grapes were too high for him.</p>
+                <p>“You have to grow up fox,” said a monkey living on the tree; “here take some grapes.”</p>
+                <p>The monkey shook the branch and a few grapes fell out. The fox caught them neatly in his mouth.</p><p>“Are they sweet?” asked the monkey.</p>
+                <p>“Not so sweet,” the fox said as he walked away.</p>
+                <p>The next day, the fox came by the tree again.</p>
+                <p>He took a look at the bunches of grapes hanging above. He took a short run and jumped. This time he could jump higher, but not high enough to get at the grapes. The friendly monkey pressed down the branch, helping the fox snatch one or two grapes as they fell.</p>
+                <p>“Is it sweet?” asked the monkey.</p>
+                <p>“Not so sweet,” said the fox as he walked away.</p>
+                <p>The next day, the fox was back.</p>
+                <p>This time he looked determined to get the grapes.</p>
+                <p>&nbsp;</p>
+                
+                <figure class="image">
+                    <img src="https://miro.medium.com/max/1169/1*qo39NEyIIvyt-5BHCYTw.jpeg">
+                </figure>
+            </div>
+        </div>
+
+        <div  class="d-flex flex-column mb-5 col-lg-8">
+            <div >
+                <img  class="rounded w-100" src="https://worldwidestorytellers.com/bongoswriters/images/fox.jpeg">
+            </div>
+            <div  class="d-flex justify-content-between align-items-center mt-3 mb-5"><div >
+                <div  class="user-pic">
+                    <img  src="http://universitiesconnect.com/bongoswriters/images/images.jpg">
+                </div>
+                <div  class="user_name">
+                    <p >Sunny</p>
+                    <p  class="auther">Auther</p>
+                </div>
+            </div>
+            <div >
+                <div  class="d-flex justify-content-between align-items-center">
+                    <div  class="share"><i  class="fas fa-share-alt"></i></div>
+                    <div >Share Stoy</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+    <?php include 'footer.php' ?>
+</body>
