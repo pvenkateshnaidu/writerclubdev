c5c7a9efa83411ed2d572bed5038e5de480ca59a
saisudhakar <muvvasaisudhakar@gmail.com>
2020-06-18 19:04:21 +0530

    commmit login header

---
+++ source/loginheader.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Writers_club</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
+    <link href="fonts/all.min.css" rel="stylesheet"> <!--load all styles -->
+</head>
+<div class="container">
+    <div  class="spacer">
+        <nav  class="navbar navbar-expand-lg navbar-dark info-color bg-white fixed-top">
+            <div  class="overlay"></div>
+            <a  class="navbar-brand"  href="#/home">
+            <img src="assets/Reg_Bingos_logo.png" />
+            </a>
+            <button  aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarSupportedContent-4" data-toggle="collapse" type="button">
+                <span  class="navbar-toggler-icon"></span>
+            </button>
+            <div  class="collapse navbar-collapse" id="navbarSupportedContent-4">
+            <div  class="user_pic active d-block d-none d-lg-none" ><a  href="#"><img src="http://universitiesconnect.com/bongoswriters/images/nature.jpg"></a></div>
+
+                <ul  class="d-flex navbar-nav ml-auto">
+                    <li  class="nav-item"><a  class="nav-link"  href="#/home">Home</a></li>
+                    <li  class="nav-item"><a  class="nav-link" href="#/storyfeed">Story Feed</a></li>
+                    <li  class="login_one nav-item"><a  class="login active" href="#/login">Login</a></li>
+                    <li  class="login_one nav-item login-spl"><a  class="login active" href="#/register">Register</a></li>
+                    <li  class="nav-item user-pic"><a href="#" class=""><img src="http://universitiesconnect.com/bongoswriters/images/nature.jpg"></a></li>
+                    <li class="nav-item drop-ellipse">
+                        <div  class="ellipsis-v-icon navbar-nav ml-auto group-info">
+                            <i  class="fas fa-ellipsis-v dropdown-toggle" data-toggle="dropdown"></i>
+                            <div class="dropdown-menu dropdown-menu-right">
+                                <a class="dropdown-item"  href="#/myprofile"><i class="profile"></i>My Profile</a>
+                                <a class="dropdown-item"  href="#/groups"><i class="mygroups"></i>My Groups</a>
+                                <a class="dropdown-item"  href="#/login"><i class="logout"></i>Log Out</a>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="nav-item d-none d-lg-none d-block "><a  class="nav-link" href="#">My Profile</a></li>
+                    <li class="nav-item d-none d-lg-none d-block "><a  class="nav-link" href="#">My Groups</a></li>
+                    <li class="nav-item d-none d-lg-none d-block "><a  class="nav-link" href="#">Logout</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
